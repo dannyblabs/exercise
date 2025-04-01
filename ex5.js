d4a6d0752446aeec0b16675f7428796ee47b0426
dannyblabs <danny@blabs.build>
2025-04-01 17:41:58 +0700

    add

--- ex5.js
+++ ex5.js
@@ -1,18 +1,29 @@
+let obj = [{
+    id: 1,
+    name: "Bee",
+    friend: ["Sam", "Tailor"],
+    photos: ["pt1.jpg", "pt2.jpg"],
+},
+{
+    id: 2,
+    name: "Danny",
+    friend: ["Loyce", "Lupin", "Bee"],
+    photos: ["pt3.jpg", "pt544.jpg", "pt5443.jpg"],
+},
+];
 
 
 function getProfile() {
     return new Promise((resolve) => {
         setTimeout(() => {
-            const obj = { id: 1, name: "Alex" }
-            resolve(`Profile: ${obj.name}`)
+            resolve(`Profile: ${obj[1].name}`)
         }, 2000);
     });
 }
 function getFriends() {
     return new Promise((resolve) => {
         setTimeout(() => {
-            const friend = ["Sam", "Tailor"];
-            resolve(`Friend: ${friend.length}`)
+            resolve(`Friend: ${obj[1].friend.length}`)
         },
             5000);
 
@@ -22,7 +33,8 @@ function getPhotos() {
     return new Promise((resolve) => {
         setTimeout(() => {
             const pt = ["pt1.jpg", "pt2.jpg"];
-            resolve(`Photos: ${pt.length}`)
+            resolve(`Photo: ${obj[1].photos.length}`)
+            //resolve(`Photos: ${pt.length}`)
         }, 1500);
     });
 }
